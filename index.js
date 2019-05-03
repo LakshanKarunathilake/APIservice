f46c17effa3c261d2ae4236593ee58bafd63654e
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-03 23:13:34 +0530

    Project Setup

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
+});
+
+// Code to state function
+app.get("/codeToState", function(req, res) {
+  console.log("Calling codeToState end point in api-service");
+  const code = req.query.code;
+  axios.get(`http://localhost:3001/codeToState?code=${code}`).then(response => {
+    console.log(response.data);
+    res.send({ state: response.data.state });
+  });
+});
+
+// State to code function
+app.get("/stateToCode", function(req, res) {
+  console.log("Calling stateToCode end point in api-service");
+  const state = req.query.state;
+  console.log("state", state);
+  axios
+    .get(`http://localhost:3001/stateToCode?state=${state}`)
+    .then(response => {
+      console.log(response.data);
+      res.send({ code: response.data.code });
+    });
 });
